1825c12ac047b0b2a84a2d4e77580113e0eb31ba
Vishal Arora <vishal@vishalarora.com>
2015-07-27 22:26:51 -0400

    Initial HeartBeat app code
    iPhone & watch app display UI with hard-coded variables.  The next step
    is to make them communicate with each other.

---
+++ HeartBeat/HeartBeat/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  HeartBeat
+//
+//  Created by Vishal on 7/27/15.
+//  Copyright (c) 2015 Code the World. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var currentHeartbeat: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculateHeartBeat()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func calculateHeartBeat() {
+        let heartbeat = 100;
+        self.currentHeartbeat.text = String(heartbeat);
+        UIViewController.
+    }
+    
+}
+
